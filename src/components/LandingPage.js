3d05b1434d29dea36ba0864506ca06d0368a0cfc
Drew Jordan <ajordan12@luc.edu>
2023-07-21 17:58:47 -0500

    updates

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import '../assets/stylesheet2.css';
+import './assets/stylesheet2.css';
 
 function LandingPage() {
     return (
         <main className="wrapper">
             <section className="typing">
-                <a href="/home" className="text-link">Hey, I'm Drew. Welcome to my portfolio.</a>
+                <a href="page2.html" className="text-link">Hey, I'm Drew. Welcome to my portfolio.</a>
             </section>
         </main>
-    );
+    )
 }
 
 export default LandingPage;
