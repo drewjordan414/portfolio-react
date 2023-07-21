540b55bd8ecfb25ada27332ffc090aa2c023aff5
Drew Jordan <ajordan12@luc.edu>
2023-07-21 18:02:55 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import HomePage from './components/HomePage'; // You would need to create this HomePage component
+import MainPage from './components/MainPage';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import About from './components/About';
 
 function App() {
     return (
@@ -9,6 +14,11 @@ function App() {
             <Switch>
                 <Route path="/" exact component={LandingPage} />
                 <Route path="/home" component={HomePage} />
+                <Route path="/resume" component={Resume} />
+                <Route path="/projects" component={Projects} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/about" component={About} />
+                <Route path="/mainpage" component={MainPage} />
                 {/* Add more Routes as necessary */}
             </Switch>
         </Router>
