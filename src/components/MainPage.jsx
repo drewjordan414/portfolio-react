c9f3b3e20066a7106b82407e1011f43e4c6d1f60
Drew Jordan <ajordan12@luc.edu>
2023-07-22 09:44:13 -0500

    updates

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -4,11 +4,13 @@ import Header from './Header';
 import AboutSection from './AboutSection';
 // import Projects from './Projects';
 import MainProject from './MainProject';
+import HomePage from './HomePage';
 import Footer from './Footer';
 import '../assets/stylesheet.css';
 
 const MainPage = () => (
   <div>
+    <HomePage />
     <Header />
     <AboutSection />
     <MainProject />
