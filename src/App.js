483dc4144b87ee1f995d1b32ccf42d9f3672c76b
Drew Jordan <ajordan12@luc.edu>
2023-07-24 22:16:11 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,34 +1,5 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import LandingPage from './components/LandingPage';
-// import HomePage from './components/HomePage'; 
-// import MainPage from './components/MainPage';
-// // import Resume from './components/Resume';
-// import MainProject from './components/MainProject';
-// // import Projects from './components/Projects';
-// // import Contact from './components/Contact';
-// // import About from './components/About';
-
-// function App() {
-//     return (
-//         <Router>
-//             <Routes>
-//                 <Route path="/" exact component={LandingPage} />
-//                 <Route path="/mainpage" component={MainPage} />
-//                 <Route path="/home" component={HomePage} />
-//                 {/* <Route path="/resume" component={Resume} /> */}
-//                 {/* <Route path="/projects" component={Projects} /> */}
-//                 <Route path="/projectsection" component={MainProject} />
-//                 {/* <Route path="/contact" component={Contact} /> */}
-//                 {/* <Route path="/about" component={About} /> */}
-//             </Routes>
-//         </Router>
-//     );
-// }
-
-// export default App;
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import HomePage from './components/HomePage'; 
 import MainPage from './components/MainPage';
