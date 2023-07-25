7a0e433640480ffca2c6f8574763dd93588b2403
Drew Jordan <ajordan12@luc.edu>
2023-07-24 21:40:26 -0500

    header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,17 +25,17 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="footer bg-light mt-auto py-3">
+    <footer className="footer bg-white mt-auto py-3">
       <div className="container">
         <ul className="nav justify-content-center">
           <li className="nav-item">
-            <a className="nav-link" href="https://github.com/drewjordan414" target="_blank" rel="noopener noreferrer">Github</a>
+            <a className="footer-nav-link" href="https://github.com/drewjordan414" target="_blank" rel="noopener noreferrer">Github</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/resume">Resume</a>
+            <a className="footer-nav-link" href="/resume">Resume</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">Home</a>
+            <a className="footer-nav-link" href="/">Home</a>
           </li>
         </ul>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,10 @@
-// import React from 'react';
-
-// function Header() {
-//   return (
-//     <header className="header" id="header">
-//         <nav className="navbar">
-//             <ul className="nav-list">
-//                 <li className="nav-item">
-//                     <a href="https://github.com/drewjordan414" target="_blank" rel="noopener noreferrer" className="github-link">Github</a>
-//                 </li>
-//                 {/* make the resume a downloadable pdf */}
-//                 <li className="nav-item">
-//                     <a href="/resume" className="resume-link">Resume</a> 
-//                 </li>
-//                 <li className="nav-item">
-//                     <a href="/" className="github-link">Home</a>
-//                 </li>
-//                 <li className="nav-item">
-//                     <a href="#projects" className="github-link">Projects</a>
-//                 </li>
-//             </ul>
-//         </nav>
-//     </header>
-//   );
-// }
-
-// export default Header;
 import React from 'react';
+import '../assets/stylesheet.css';
 
 function Header() {
   return (
     <header id="header">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-white">
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -40,7 +14,6 @@ function Header() {
                         <li className="nav-item">
                             <a className="nav-link" href="https://github.com/drewjordan414" target="_blank" rel="noopener noreferrer">Github</a>
                         </li>
-                        {/* make the resume a downloadable pdf */}
                         <li className="nav-item">
                             <a className="nav-link" href="/resume">Resume</a> 
                         </li>
@@ -59,4 +32,3 @@ function Header() {
 }
 
 export default Header;
-
