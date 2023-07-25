9cbd0a87fcdc3418090d9a2560c21025a50aa100
Drew Jordan <ajordan12@luc.edu>
2023-07-24 23:30:05 -0500

    icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,28 @@
+// import React from 'react';
+
+// function Footer() {
+//   return (
+//     <footer className="footer bg-white mt-auto py-3">
+//       <div className="container">
+//         <ul className="nav justify-content-center">
+//           <li className="nav-item">
+//             <a className="footer-nav-link" href="https://github.com/drewjordan414" target="_blank" rel="noopener noreferrer">Github</a>
+//           </li>
+//           <li className="nav-item">
+//             <a className="footer-nav-link" href="/resume">Resume</a>
+//           </li>
+//           <li className="nav-item">
+//             <a className="footer-nav-link" href="/">Home</a>
+//           </li>
+//         </ul>
+//       </div>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
 import React from 'react';
+import { Github, Envelope, Linkedin } from 'react-bootstrap-icons';
 
 function Footer() {
   return (
@@ -6,13 +30,19 @@ function Footer() {
       <div className="container">
         <ul className="nav justify-content-center">
           <li className="nav-item">
-            <a className="footer-nav-link" href="https://github.com/drewjordan414" target="_blank" rel="noopener noreferrer">Github</a>
+            <a className="footer-nav-link" href="https://github.com/drewjordan414" target="_blank" rel="noopener noreferrer">
+              <Github />
+            </a>
           </li>
           <li className="nav-item">
-            <a className="footer-nav-link" href="/resume">Resume</a>
+            <a className="footer-nav-link" href="mailto:drewjordan404@gmail.com">
+              <Envelope />
+            </a>
           </li>
           <li className="nav-item">
-            <a className="footer-nav-link" href="/">Home</a>
+            <a className="footer-nav-link" href="https://www.linkedin.com/in/drew-jordan-103993221/" target="_blank" rel="noopener noreferrer">
+              <Linkedin />
+            </a>
           </li>
         </ul>
       </div>
